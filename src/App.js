bbc22f769d20daac154f81bdf54bc560abe04873
linbim <linbim@yahoo.com>
2022-08-05 18:08:40 +0100

    file update
    added react app source code with two logo

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import dog from './logos/dog.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={dog} className="App-dog" alt="dog" style={{position:'absolute', left: 5, top: 5, }} />
+        <p>
+         welcome to my new app!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
